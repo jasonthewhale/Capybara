a0585b6496ebc6815781d02d126ac11191285fed
赵丹阳 <zhaodanyang@zhaodanyangdeMacBook-Pro-3.local>
2023-08-27 13:00:34 +1000

    count number

--- countdown.js
+++ countdown.js
@@ -2,10 +2,32 @@ let oldBody = document.body.cloneNode(true);
 const pureNumber = /^\d+$/;
 const countdown = /(?:\d{1,2}\s*:\s*){1,3}\d{1,2}|(?:\d{1,2}\s*(?:days?|hours?|minutes?|seconds?|[a-zA-Z]{1,3}\.?)\s*){2,4}/gi;
 const notCountdown = /(?:\d{1,2}\s*:\s*){4,}\d{1,2}|(?:\d{1,2}\s*(?:days?|hours?|minutes?|seconds?|[a-zA-Z]{1,3}\.?)\s*){5,}/gi;
+let countdown_value = 0;
 
 setInterval(() => {
+  countdown_value = 0;
   traverseDOM(oldBody, document.body);
-}, 5000);  // check every 1s
+
+  console.log("countdown_value", countdown_value);
+
+  const riskElement = document.getElementById('riskLevel');
+  const riskImageElement = document.getElementById('riskImage');
+  const hiddenRisk = document.getElementById('hiddenSum');
+  const checkboxRisk = document.getElementById('checkboxSum');
+  
+  hiddenRisk.textContent = "sum: " + hidden_count;
+  checkboxRisk.textContent = "sum: " + checkbox_count;
+  
+  if (countdown_value > 15) {
+    riskImageElement.src = 'high-risk.jpg';
+    riskElement.style.color = '#d81e06';
+    riskElement.textContent = "High Risk";
+  }else if (countdown_value > 5) {
+    riskImageElement.src = 'middle-risk.jpg';
+    riskElement.style.color = '#f4ea2a';
+    riskElement.textContent = "Moderate Risk";
+  }
+}, 5000);  // check every 5s
 
 function traverseDOM(oldNode, node) {
   var children = node.childNodes;
@@ -26,6 +48,7 @@ function traverseDOM(oldNode, node) {
                 if (countdown.test(allTexts) && !notCountdown.test(allTexts)) {
                     children[i].parentNode.parentNode.style.backgroundColor = "red";
                     console.log("found countdown", allTexts);
+                    countdown_value++;
                 }
             }
         }
