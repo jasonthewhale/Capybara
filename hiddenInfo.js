a545f3f9fb9b411d958d1582c91afb0378eabbe8
jasonthewhale <youjunchen000220@gmail.com>
2023-08-27 12:32:11 +1000

    merge hidden check into countdown.js

--- hiddenInfo.js
+++ hiddenInfo.js
@@ -21,10 +21,10 @@ function traverseDOM(oldNode, node) {
         var fontSize = window.getComputedStyle(children[i].parentNode, null).getPropertyValue('font-size');
         var display = window.getComputedStyle(children[i].parentNode, null).getPropertyValue('display');
         var visibility = window.getComputedStyle(children[i].parentNode, null).getPropertyValue('visibility');
-        var bgColor = window.getComputedStyle(links[i].parentNode).getPropertyValue('background-color');
+        var bgColor = window.getComputedStyle(children[i].parentNode).getPropertyValue('background-color');
 
         if (parseInt(fontSize) <= 12 && parent.hasAttribute('href')) {
-            console.log(parent.getAttribute('href'));
+            console.log(parent.tagName);
         }
   
         // Check if font size is less than a certain size or if text is hidden
