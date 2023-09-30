689435391505a42be4dd98be910937b4799ad5c8
Alaurant <zhaody085@163.com>
2023-09-30 22:27:55 +1000

    Flora part

---
+++ website/html/aboutUs.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/stylesheet.css">
+    <script src="script.js"></script>
+    <title>About Us</title>
+</head>
+
+    
+<body>
+
+    <nav class="navbar">
+        <div class="logo">
+            <a href="../html/">
+                <img src="../img/logo.png" alt="Logo"> 
+            </a>
+        </div>
+        <ul class="nav-list">
+            <li><a href="../html/user_guide.php">User Guide & User Journey</a></li>
+            <li><a href="../html/darkpatterns.php">Dark Patterns & Examples</a></li>
+            <li><a href="../html/user_reporting.php">User Reporting</a></li>
+            <li><a href="../html/aboutUs.php">About Us </a></li>
+        </ul>
+    </nav>
+
+    <div class="about-container">
+        <article class="article-container">
+
+            <div class="text-container">
+                <h2>About  our  team</h2>
+                <h3>Team  goals  and  collaborations  info</h3>
+                <p>Team Goal:
+                    Our team's primary goal is to deliver exceptional results in [specific project or objective] by [deadline]. We aim to achieve this by leveraging each team member's unique skills and expertise to contribute to the project's success. Our overarching objective is to [describe the broader purpose or impact of the project]. We will measure our success based on key performance indicators (KPIs) such as [list relevant KPIs].
+                </p>
+                <hr>
+                <h3>Team  members  info:</h3>
+
+                <ul class="dot-list">
+                    <div class="child-list"> 
+                        <li class="dot-list-item">&#8226; team member 1</li>
+                        <li class="dot-list-item">&#8226; team member 2</li>
+                        <li class="dot-list-item">&#8226; team member 3</li>
+                    </div>
+
+                    <div class="child-list"> 
+                        <li class="dot-list-item">&#8226; team member 4</li>
+                        <li class="dot-list-item">&#8226; team member 5</li>
+                        <li class="dot-list-item">&#8226; team member 6</li>
+                    </div>
+                </ul>
+                <P style="color:transparent">The problem space of sustainable urban transportation encompasses various challenges 
+                    related to reducing the environmental impact of transportation systems within cities.
+                </p>
+            </div>
+            <br>
+
+            <div class="text-container">
+                <h2>About Our Project</h2>
+                <h3>Project Overview + Problem Space</h3>
+                <P>The problem space of sustainable urban transportation encompasses various challenges 
+                    related to reducing the environmental impact of transportation systems within cities.
+                </p>
+                <P>The problem space of sustainable urban transportation encompasses various challenges 
+                    related to reducing the environmental impact of transportation systems within cities.
+                </p>
+                <hr>
+                <h3>Design Process (Timeline):</h3>
+                <br>
+                <div class="mst-container" style="margin-left: 10%;">
+                    <img src="../img/milestone.png">
+
+                </div>
+
+                <P style="color:transparent">The problem space of sustainable urban transportation encompasses various challenges 
+                    related to reducing the environmental impact of transportation systems within cities.
+                </p>
+            </div>
+
+            
+
+        </article>
+        
+    </div>
+    
+    <footer>
+        <div class="footer-content">
+            <p>&copy; 2023 Shadow Hunter</p>
+        </div>
+    </footer>
+    
+
+</body>
+</html>
+    
